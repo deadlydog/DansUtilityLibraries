dab1caed2f1d1c929d640b71aee482f0fb1b05c5
deadlydog <deadlydog@hotmail.com>
2012-06-29 10:41:35 -0600

    First commit. Creating solution and adding first few classes :)

---
+++ DansUtilityLibraries/DansWPFLibrary/Commands/CommandBase.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DansWPFLibrary.Commands
+{
+    public class CommandBase : ICommand
+    {
+        public event EventHandler HasExecuted;
+
+        #region Fields
+
+        readonly Action<object> _execute;
+        private readonly Action _executeNoParams;
+        readonly Predicate<object> _canExecute;
+
+        #endregion // Fields
+
+        #region Constructors
+
+        public CommandBase(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        public CommandBase(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the RelayCommand class that 
+        /// can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <exception cref="ArgumentNullException">If the execute argument is null.</exception>
+        public CommandBase(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the RelayCommand class.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        /// <exception cref="ArgumentNullException">If the execute argument is null.</exception>
+        public CommandBase(Action execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            _executeNoParams = execute;
+            _canExecute = canExecute;
+        }
+        #endregion // Constructors
+
+        #region ICommand Members
+
+        [System.Diagnostics.DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_execute != null)
+                _execute(parameter);
+            else if (_executeNoParams != null)
+                _executeNoParams();
+        }
+
+        #endregion // ICommand Members
+
+        protected void RaiseHasExecutedEvent()
+        {
+            if (HasExecuted != null)
+                HasExecuted(this, new EventArgs());
+        }
+    }
+}
---
+++ DansUtilityLibraries/DansWPFLibrary/Converters/StringIsNotNullOrWhitespaceToBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DansWPFLibrary.Converters
+{
+    public class StringIsNotNullOrWhitespaceToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !string.IsNullOrWhiteSpace(value.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
