ddcb6fe8e9aceb55c7bfc4fc9751a1e3e8a73d2f
deadlydog <deadlydog@hotmail.com>
2014-01-29 16:59:30 -0600

    - Updated exception helper.
    - Turned on documentation xml file generation.

--- DansUtilityLibraries/DansCSharpLibrary/Exceptions/ExceptionHelper.cs
+++ DansUtilityLibraries/DansCSharpLibrary/Exceptions/ExceptionHelper.cs
@@ -21,21 +21,25 @@ namespace DansCSharpLibrary.Exceptions
 		/// <para>If a Debugger is not attached, GetExceptionMessages(ex, innerExceptionMessageSeparator) is returned. This is a more user-friendly error message.</para>
 		/// </summary>
 		/// <param name="ex">The exception to process.</param>
+		/// <param name="removeSeeInnerExceptionForDetailsStringsFromMessage">If true all occurrences of "See the inner exception for details." will be removed from the returned string.</param>
 		/// <param name="innerExceptionMessageSeparator">The separator to use to separate each inner exception's message in the returned string.</param>
-		public static string GetExceptionMessagesBasedOnDebugging(Exception ex, string innerExceptionMessageSeparator = DEFAULT_INNER_EXCEPTION_MESSAGE_SEPARATOR)
+		/// <returns></returns>
+		public static string GetExceptionMessagesBasedOnDebugging(Exception ex, bool removeSeeInnerExceptionForDetailsStringsFromMessage = true, string innerExceptionMessageSeparator = DEFAULT_INNER_EXCEPTION_MESSAGE_SEPARATOR)
 		{
 			if (Debugger.IsAttached)
 				return ex.ToString();
 			else
-				return GetExceptionMessages(ex, innerExceptionMessageSeparator);
+				return GetExceptionMessages(ex, removeSeeInnerExceptionForDetailsStringsFromMessage, innerExceptionMessageSeparator);
 		}
 
 		/// <summary>
 		/// Gets the exception's message, along with all inner exception messages. This is typically a more user-friendly error message than ex.ToString().
 		/// </summary>
 		/// <param name="ex">The exception to process.</param>
+		/// <param name="removeSeeInnerExceptionForDetailsStringsFromMessage">If true all occurrences of "See the inner exception for details." will be removed from the returned string.</param>
 		/// <param name="innerExceptionMessageSeparator">The separator to use to separate each inner exception's message in the returned string.</param>
-		public static string GetExceptionMessages(Exception ex, string innerExceptionMessageSeparator = DEFAULT_INNER_EXCEPTION_MESSAGE_SEPARATOR)
+		/// <returns></returns>
+		public static string GetExceptionMessages(Exception ex, bool removeSeeInnerExceptionForDetailsStringsFromMessage = true, string innerExceptionMessageSeparator = DEFAULT_INNER_EXCEPTION_MESSAGE_SEPARATOR)
 		{
 			var messages = new StringBuilder();
 			messages.Append(ex.Message);
@@ -44,6 +48,11 @@ namespace DansCSharpLibrary.Exceptions
 				ex = ex.InnerException;
 				messages.Append(innerExceptionMessageSeparator + ex.Message);
 			}
+
+			// Erase all references to checking the inner exception for details, as that's not very user-friendly.
+			if (removeSeeInnerExceptionForDetailsStringsFromMessage)
+				messages = messages.Replace(" See the inner exception for details.", string.Empty);
+
 			return messages.ToString();
 		}
 	}
