913f536f7441047569c2a7335453619ad876770e
deadlydog <deadlydog@hotmail.com>
2015-07-08 18:11:06 -0600

    Added helper function for reading settings from app/web.config files.

---
+++ DansUtilityLibraries/DansCSharpLibrary/Configuration/ConfigurationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace DansCSharpLibrary.Configuration
+{
+	public class ConfigurationManager
+	{
+		/// <summary>
+		/// Gets the value from application settings from the app/web.config file, or throws a System.Configuration.ConfigurationErrorsException exception if it is not found.
+		/// </summary>
+		/// <param name="appSettingKey">Name of the application setting key in the app/web.config file.</param>
+		/// <returns></returns>
+		/// <exception cref="System.Configuration.ConfigurationErrorsException"></exception>
+		public static string GetValueFromAppSettingsOrThrowException(string appSettingKey)
+		{
+			if (System.Configuration.ConfigurationManager.AppSettings[appSettingKey] == null)
+				throw new ConfigurationErrorsException(string.Format("Could not find the application setting '{0}' in the app.config file.", appSettingKey));
+			return System.Configuration.ConfigurationManager.AppSettings[appSettingKey];
+		}
+	}
+}
