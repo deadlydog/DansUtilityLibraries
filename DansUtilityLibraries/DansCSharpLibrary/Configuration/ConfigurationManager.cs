320091da57245377b71f15f087d361be9e65676f
deadlydog <deadlydog@hotmail.com>
2015-10-08 16:31:14 -0600

    Added more ConfigurationManager helper functions and pushed new version of NuGet package.

--- DansUtilityLibraries/DansCSharpLibrary/Configuration/ConfigurationManager.cs
+++ DansUtilityLibraries/DansCSharpLibrary/Configuration/ConfigurationManager.cs
@@ -45,5 +45,45 @@ namespace DansCSharpLibrary.Configuration
 				throw new System.Configuration.ConfigurationErrorsException(errorMessage, ex);
 			}
 		}
+
+		/// <summary>
+		/// Gets the boolean value of a key in the appSettings section of the app/web.config file, or throws a System.Configuration.ConfigurationErrorsException exception if it is not found or cannot be converted to a boolean.
+		/// </summary>
+		/// <param name="appSettingKey">Name of the application setting key in the app/web.config file.</param>
+		/// <returns></returns>
+		/// <exception cref="System.Configuration.ConfigurationErrorsException"></exception>
+		public static bool GetBoolValueFromConfigurationManagerAppSettings(string appSettingKey)
+		{
+			var stringValue = GetValueFromConfigurationManagerAppSettings(appSettingKey);
+			try
+			{
+				return bool.Parse(stringValue);
+			}
+			catch (Exception ex)
+			{
+				var errorMessage = string.Format("Could not convert the value '{0}' of the appSetting key '{1}' in the app/web.config to a boolean.", stringValue, appSettingKey);
+				throw new System.Configuration.ConfigurationErrorsException(errorMessage, ex);
+			}
+		}
+
+		/// <summary>
+		/// Gets the connection string of a name in the connectionStrings section of the app/web.config file, or throws a System.Configuration.ConfigurationErrorsException exception if it is not found.
+		/// </summary>
+		/// <param name="connectionStringName">Name of the connection string in the app/web.config file.</param>
+		/// <returns></returns>
+		/// <exception cref="System.Configuration.ConfigurationErrorsException"></exception>
+		public static string GetConnectionStringFromConfigurationManagerConnectionStrings(string connectionStringName)
+		{
+			try
+			{
+				var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings[connectionStringName];
+				return connectionString.ConnectionString;
+			}
+			catch (Exception ex)
+			{
+				var errorMessage = string.Format("Could not find the connectionString name '{0}' in the app/web.config file.", connectionStringName);
+				throw new System.Configuration.ConfigurationErrorsException(errorMessage, ex);
+			}
+		}
 	}
 }
