308a2c1482dc1023ab5de0671615a61902ecd27e
deadlydog <deadlydog@hotmail.com>
2017-07-18 18:23:03 -0600

    Fixing function to ignore properties that require arguments

--- src/DansCSharpLibrary/Diagnostics/ReflectionBasedDiagnosticInformationProvider.cs
+++ src/DansCSharpLibrary/Diagnostics/ReflectionBasedDiagnosticInformationProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,7 +24,7 @@ namespace DansCSharpLibrary.Diagnostics
 			var infoBuilder = new StringBuilder();
 
 			// Aggregate all of the public properties into a comma-separated string of name=value pairs.
-			var publicProperties = _instance.GetType().GetProperties().Where(p => p.PropertyType.IsPublic);
+			var publicProperties = _instance.GetType().GetProperties().Where(PropertyShouldBeListedInDiagnosticInfo);
 			foreach (var property in publicProperties)
 			{
 				infoBuilder.Append($"{property.Name}='{property.GetValue(_instance)}',");
@@ -33,6 +34,20 @@ namespace DansCSharpLibrary.Diagnostics
 			return string.IsNullOrWhiteSpace(info) ?
 				$"No diagnostic info to report on type '{_instance.GetType().FullName}' because no public properties were found." :
 				$"Instance of type '{_instance.GetType().FullName}' has the following public property values (format is name='value'): {info}";
+		}
+
+		private static bool PropertyShouldBeListedInDiagnosticInfo(PropertyInfo property)
+		{
+			return property.PropertyType.IsPublic && PropertyHasNoParameters(property);
+		}
+
+		/// <remarks>
+		/// A property with parameters is an indexer, such as <see cref="System.Data.DataRow.this[System.Data.DataColumn]"/>.
+		/// </remarks>
+		private static bool PropertyHasNoParameters(PropertyInfo property)
+		{
+			var indexParameters = property.GetIndexParameters();
+			return indexParameters == null || !indexParameters.Any();
 		}
 	}
 }
